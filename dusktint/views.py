4ee56279b16ec4d57b24761dd3f8f3b1b7b8ff7c
meijii <heyoones@gmail.com>
2019-11-19 19:37:11 +1300

    Updated site font to sans-serif "Work Sans" from google fonts.
    Updated footer to only include social media links

--- dusktint/views.py
+++ dusktint/views.py
@@ -12,7 +12,7 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='dusktint',
+        title='Elloise Sabdao @dusktint',
         year=datetime.now().year,
     )
 
